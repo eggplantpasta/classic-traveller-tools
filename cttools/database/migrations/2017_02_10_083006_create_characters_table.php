69407c39222d517f2efc0615e7631e04ff11a27c
Brett Roper <roper.brett@gmail.com>
2017-03-17 07:36:24 +0800

    added further character attributes

--- cttools/database/migrations/2017_02_10_083006_create_characters_table.php
+++ cttools/database/migrations/2017_02_10_083006_create_characters_table.php
@@ -16,15 +16,33 @@ class CreateCharactersTable extends Migration
         Schema::create('characters', function (Blueprint $table) {
             $table->increments('id');
 
+            // personal details
             $table->string('name', 100);
             $table->string('title', 20)->nullable();
+            $table->smallInteger('age')->unsigned()->default(18);
             $table->string('sex', 1)->nullable();
+
+            // service history
+            $table->string('service', 20)->nullable();
+            $table->string('rank', 20)->nullable();
+            $table->smallInteger('terms_of_service')->unsigned()->default(0);
+            $table->boolean('retired')->default(FALSE);
+
+            // UPP
             $table->smallInteger('strength')->unsigned();
             $table->smallInteger('dexterity')->unsigned();
             $table->smallInteger('endurance')->unsigned();
             $table->smallInteger('intelligence')->unsigned();
             $table->smallInteger('education')->unsigned();
             $table->smallInteger('social')->unsigned();
+
+            // assets
+            $table->smallInteger('credits')->unsigned()->default(0);
+            $table->smallInteger('retirement_pay')->unsigned()->default(0);
+            $table->smallInteger('high_passage')->unsigned()->default(0);
+            $table->smallInteger('middle_passage')->unsigned()->default(0);
+            $table->smallInteger('low_passage')->unsigned()->default(0);
+            $table->boolean('tas_membership')->default(FALSE);
 
             $table->timestamps();
         });

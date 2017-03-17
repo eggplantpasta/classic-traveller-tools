00f15115e7a4299c867a8e9b5c7f3b1e915349f3
Brett Roper <roper.brett@gmail.com>
2017-03-17 08:52:19 +0800

    added static function for die throws

---
+++ cttools/app/Cttools/Game.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Cttools;
+
+class Game
+{
+    public static function throwD6(int $count = 2)
+    {
+        for ($i = 1; $i <= $count; $i++) {
+            $sum += rand(1, 6);
+        }
+        return $sum;
+    }
+}
